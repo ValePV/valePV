15b3a095efd35a133bfd9b55af5db5c6c57ba38e
valePV <va.perez19@hotmail.com>
2018-03-11 14:28:31 -0300

    version movil

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,41 @@
-$(document).ready(function(){
-  $('.header').show();
-  $('.section-about').hide();
-  $('.section-work').hide();
-
-
-   
-  $('.logo-princ').click(function(){
+window.onload = function() {
+  
+  if(screen.width >= 992) {
+    // inicio
     $('.header').show();
     $('.section-about').hide();
     $('.section-work').hide();
-  });
 
-  $('#button-next').click(function(){
-    $('.header').hide();
-    $('.section-about').show();
-    $('.section-work').hide();
-  });  
-  $('.go-work').click(function(){
-    $('.header').hide();
-    $('.section-about').hide();
-    $('.section-work').show();
-  });   
-  
+    //al clickear el logo volver a home
+    $('.logo-princ').click(function(){
+      $('.header').show();
+      $('.section-about').hide();
+      $('.section-work').hide();
+    });
 
-  //intervalos en n segundos
-  setInterval(function(){
-    moveRight();
-  }, 2000);
+    // al hacer click en boton about, ir a la segunda sección
+    $('#button-next').click(function(){
+      $('.header').hide();
+      $('.section-about').show();
+      $('.section-work').hide();
+    });
+
+    // al hacer click en link de ver trabajo, ir a la sección tres
+    $('.go-work').click(function(){
+      $('.header').hide();
+      $('.section-about').hide();
+      $('.section-work').show();
+    });
+
+     //slider de skills
+    // intervalos en n segundos
+    setInterval(function(){
+      moveRight();
+    }, 2000);
   
-  var slideWidth = $('#slider ul li').width();
-	
-  function moveRight() {
+    var slideWidth = $('#slider ul li').width();
+  
+    function moveRight() {
     $('#slider ul').animate({
       left: - slideWidth
     }, 200, function () {
@@ -38,23 +43,81 @@ $(document).ready(function(){
         $('#slider ul').css('left', '');
       });
     };
-});
 
-$(function() {
-    $('#accordion > li').hover(
+
+    $(function() {
+      $('#accordion > li').hover(
         function () {
             var $this = $(this);
-            $this.stop().animate({'width':'480px'},500);
+            $this.stop().animate({'width':'400px'},500);
             $('.head',$this).stop(true,true).fadeOut();
             $('.about',$this).stop(true,true).slideDown(500);
             $('.more',$this).stop(true,true).fadeIn();
         },
         function () {
             var $this = $(this);
-            $this.stop().animate({'width':'200px'},1000);
+            $this.stop().animate({'width':'100px'},1000);
             $('.head',$this).stop(true,true).fadeIn();
             $('.more',$this).stop(true,true).fadeOut(500);
             $('.about',$this).stop(true,true).slideUp(700);
         }
-    );
-});
+      );
+    });
+
+    } else {
+      // toggle class scroll 
+      $(window).scroll(function() {
+        if($(this).scrollTop() > 50) {
+          $('.navbar-trans').addClass('afterscroll');
+        } else {
+          $('.navbar-trans').removeClass('afterscroll');
+        }  
+
+      });
+      //slider de skills
+      // intervalos en n segundos
+      setInterval(function(){
+        moveRight();
+      }, 2000);
+    
+      var slideWidth = $('#slider ul li').width();
+    
+      function moveRight() {
+      $('#slider ul').animate({
+        left: - slideWidth
+      }, 200, function () {
+          $('#slider ul li:first-child').appendTo('#slider ul');
+          $('#slider ul').css('left', '');
+        });
+      };
+      
+      $('.but-ab').click(function(){
+        $('.picture').addClass('animated zoomInDown');
+        $('.cont-about').addClass('animated fadeIn');
+      });
+
+      $('.but-ab').click(function(){
+        $('.cont-work').addClass('animated fadeIn');
+      });
+
+      
+
+
+
+    }
+
+  
+
+}
+  
+ 
+
+
+
+    
+
+    
+  
+  
+
+  
