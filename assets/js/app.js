91635bbe67af27b0cc860c2da42a38439c95ca98
valePV <va.perez19@hotmail.com>
2018-03-19 22:21:09 -0300

    proyects

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,27 @@ window.onload = function() {
   
   if(screen.width >= 992) {
     // inicio
-    $('.header').show();
+    $('.home-section').show();
     $('.section-about').hide();
     $('.section-work').hide();
 
     //al clickear el logo volver a home
     $('.logo-princ').click(function(){
-      $('.header').show();
+      $('.home-section').show();
       $('.section-about').hide();
       $('.section-work').hide();
     });
 
     // al hacer click en boton about, ir a la segunda sección
     $('#button-next').click(function(){
-      $('.header').hide();
+      $('.home-section').hide();
       $('.section-about').show();
       $('.section-work').hide();
     });
 
     // al hacer click en link de ver trabajo, ir a la sección tres
     $('.go-work').click(function(){
-      $('.header').hide();
+      $('.home-section').hide();
       $('.section-about').hide();
       $('.section-work').show();
     });
@@ -45,44 +45,31 @@ window.onload = function() {
     };
 
 
-    $(function() {
-      $('#accordion > li').hover(
-        function () {
-            var $this = $(this);
-            $this.stop().animate({'width':'400px'},500);
-            $('.head',$this).stop(true,true).fadeOut();
-            $('.about',$this).stop(true,true).slideDown(500);
-            $('.more',$this).stop(true,true).fadeIn();
-        },
-        function () {
-            var $this = $(this);
-            $this.stop().animate({'width':'100px'},1000);
-            $('.head',$this).stop(true,true).fadeIn();
-            $('.more',$this).stop(true,true).fadeOut(500);
-            $('.about',$this).stop(true,true).slideUp(700);
-        }
-      );
-    });
+    $(".hover").mouseleave(
+      function () {
+        $(this).removeClass("hover");
+      }
+    );
 
-    } else {
+  } else {
       // toggle class scroll 
-      $(window).scroll(function() {
-        if($(this).scrollTop() > 50) {
-          $('.navbar-trans').addClass('afterscroll');
-        } else {
-          $('.navbar-trans').removeClass('afterscroll');
-        }  
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 50) {
+        $('.navbar-trans').addClass('afterscroll');
+      } else {
+        $('.navbar-trans').removeClass('afterscroll');
+      }  
 
-      });
+    });
       //slider de skills
       // intervalos en n segundos
-      setInterval(function(){
-        moveRight();
-      }, 2000);
+    setInterval(function(){
+      moveRight();
+    }, 2000);
     
-      var slideWidth = $('#slider ul li').width();
+    var slideWidth = $('#slider ul li').width();
     
-      function moveRight() {
+    function moveRight() {
       $('#slider ul').animate({
         left: - slideWidth
       }, 200, function () {
